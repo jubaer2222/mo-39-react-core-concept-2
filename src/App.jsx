ecbf38e2302055f2f738e44f6e497764246d92fb
jubaer2222 <139365401+jubaer2222@users.noreply.github.com>
2025-08-16 18:20:38 +0600

    api call using use

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Batsman from './Batsman'
 import Counter from './Counter'
+import Users from './Users'
+import Friends from './Friends'
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res => res.json())
+
+ const fetchFriends = async()=>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json();
+ } 
 
 function App() {
+  const friendsPromise = fetchFriends();
    
   function handleClick(){
     alert('I am clicked.')
@@ -22,6 +34,15 @@ function App() {
     <>
       
       <h1>Vite + React</h1>
+
+      <Suspense fallback={<h3>Friends are coming for the treat...</h3>}>
+         <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
+{/* 
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers ={fetchUsers}></Users>
+      </Suspense> */}
+
       <Batsman></Batsman>
       <Counter></Counter>
      <button onClick={handleClick}>Click Me</button>
